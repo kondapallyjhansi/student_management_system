5a4bfcebf14222efba7722d16ef223e030cb8e61
kondapallyjhansi <kondapallyjhansi@gmail.com>
2026-02-28 17:40:25 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,5 +1,26 @@
 from database import connect_db
 
+# Register User
+def register(username, password):
+    conn = connect_db()
+    cur = conn.cursor()
+
+    # Check if username exists
+    cur.execute("SELECT * FROM users WHERE username=?", (username,))
+    if cur.fetchone():
+        conn.close()
+        return False  # Already exists
+
+    cur.execute(
+        "INSERT INTO users(username,password) VALUES(?,?)",
+        (username, password)
+    )
+    conn.commit()
+    conn.close()
+    return True
+
+
+# Login User
 def login(username, password):
     conn = connect_db()
     cur = conn.cursor()
